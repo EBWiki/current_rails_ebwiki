643145f3dc72f6ee90c17ca76bda116fb385a707
Mark Nyon <msnyon@hey.com>
2024-02-26 07:20:38 -0500

    WIP: Copying layout from older EBWiki

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,12 @@ Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
+  root 'static#about'
   # root "posts#index"
+  get '/about', to: 'static#about'
+  get '/guidelines', to: 'static#guidelines'
+  get '/javascript_lab', to: 'static#javascript_lab'
+  get '/instructions', to: 'static#instructions'
+  get '/get-involved', to: 'static#further_actions'
+  get '/how-to-help', to: 'static#how_to_help'
 end
